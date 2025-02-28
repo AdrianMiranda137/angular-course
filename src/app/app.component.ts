5994dc08c6dbd0dd1e5ff6141a4b690946423e88
Adrian Miranda <adrian.miranda137@gmail.com>
2025-02-27 23:40:12 -0600

    Clase 39

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  standalone: false,
 })
 export class AppComponent {
-  public title: string = 'Hola mundo';
+  title = 'bases';
 }
